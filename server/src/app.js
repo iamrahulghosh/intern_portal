b66ae202d07aa479d83ecf7de51016f8b2f6e137
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-07-28 23:59:59 +0530

    TEMP

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://cxi-student-portal.vercel.app/",
     methods: [ "GET", "POST", "PUT", "PATCH", "DELETE" ],
     credentials: true
 }))
